b7aa7f917504d14e4f13666a7278d862d238cce6
KonstantinFessaliysky <kfessalijskij@gmail.com>
2024-12-13 17:16:04 +0300

    Update Main.java
    Finding the biggest number in "main"

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,24 @@
-package org.example;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
+public class Program{
 
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
+    public static void main (String args[]){
 
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int num3 = in.nextInt();
+        int num4 = in.nextInt();
 
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        if(num2>num1){
+            num1=num2;
+        }
+        if(num3>num1){
+            num1=num3;
+        }
+        if(num4>num1){
+            num1=num4;
+        }
+        System.out.println("The biggest number is " + num1);
     }
 }
